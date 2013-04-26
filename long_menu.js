661189ffa546d40ae9a4cb8e389986427dbd0a6d
Ryan Buckley <arebuckley+1@gmail.com>
2013-04-25 14:22:34 -1000

    Update long_menu.js

--- long_menu.js
+++ long_menu.js
@@ -1,3 +1,17 @@
+/**
+ * Works on a markup that looks something like this (only longer):
+ *  <ul class="dropdown-toggle controlled">
+ *     <li>Header</li>
+ *     <li class="controllable"><a href="#">Item 1</a></li>
+ *     <li class="controllable"><a href="#">Item 2</a></li>
+ *     <li class="controllable"><a href="#">Item 3</a></li>
+ *     <li class="controllable"><a href="#">Item 4</a></li>
+ *     <li class="controllable"><a href="#">Item 5</a></li>
+ *     <li class="controllable"><a href="#">Item 6</a></li>
+ *  </ul>
+ * @constructor
+ */
+
 function LongMenu() {
     var menu = this;
 
