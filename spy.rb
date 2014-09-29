1910d05143163457329a53f407363466d1925335
Ryan Buckley <arebuckley+1@gmail.com>
2014-09-29 14:19:27 -0700

    Update spy.rb

--- spy.rb
+++ spy.rb
@@ -1,6 +1,6 @@
 module Spy
   def self.extended(base)
-    (base.methods - Object.methods).each do |method_name|
+    (base.methods - [:object_id, :__send__]).each do |method_name|
       module_eval <<-RUBY
       def #{method_name}(*args)
         puts "Called #{method_name}"
